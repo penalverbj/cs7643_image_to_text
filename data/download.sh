b9267f8c19b8b754359f102e53aec62932736ef7
alexpan82 <apan82@gmail.com>
2023-04-15 14:44:03 -0400

    Removing zip files

--- data/download.sh
+++ data/download.sh
@@ -30,8 +30,10 @@ if [ ! -d "./coco" ]; then
 
     curl -O http://images.cocodataset.org/zips/train2017.zip
     unzip -q train2017.zip
+    rm train2017.zip
     curl -O http://images.cocodataset.org/annotations/annotations_trainval2017.zip
     unzip -q annotations_trainval2017.zip
+    rm annotations_trainval2017.zip
 
     cd ..
 fi
