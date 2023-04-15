24f32e818d2ed343f30c13e754837257125645e2
jpb6 <jpb6@gatech.edu>
2023-04-15 18:08:15 -0400

    manual coco loaders work

--- src/dataHelpers/cocoParser.py
+++ src/dataHelpers/cocoParser.py
@@ -17,8 +17,8 @@ class cocoParser:
             self.annId_dict[ann['id']] = ann
         for img in coco['images']:
             self.im_dict[img['id']] = img
-        for cat in coco['categories']:
-            self.cat_dict[cat['id']] = cat
+        # for cat in coco['categories']:
+        #     self.cat_dict[cat['id']] = cat
         for license in coco['licenses']:
             self.licenses_dict[license['id']] = license
 
@@ -33,9 +33,9 @@ class cocoParser:
         im_ids = ann_ids if isinstance(ann_ids, list) else [ann_ids]
         return [self.annId_dict[ann_id] for ann_id in ann_ids]
 
-    def load_cats(self, class_ids):
-        class_ids = class_ids if isinstance(class_ids, list) else [class_ids]
-        return [self.cat_dict[class_id] for class_id in class_ids]
+    # def load_cats(self, class_ids):
+    #     class_ids = class_ids if isinstance(class_ids, list) else [class_ids]
+    #     return [self.cat_dict[class_id] for class_id in class_ids]
 
     def get_imgLicenses(self, im_ids):
         im_ids = im_ids if isinstance(im_ids, list) else [im_ids]
