c92201bed167cd8f184a543fc9fc3d1d3632d1d5
alexpan82 <apan82@gmail.com>
2023-04-15 17:59:46 -0400

    Finished init

--- src/encoder.py
+++ src/encoder.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import git
+import sys
 
 class Encoder(nn.Module):
     """
@@ -11,4 +12,9 @@ class Encoder(nn.Module):
 def __init__(self, hidden_dim=128):
     super(Encoder, self).__init__()
     home_dir = git.Repo('.', search_parent_directories=True).working_tree_dir
+    sys.path.append(home_dir + '/models/TinyViT')
     self.model = torch.load(home_dir + '/models/TinyViT/tinyvit_21M.pt')
+
+    # self.models.patch_embed
+    # self.models.layers
+    # self.models.layers[0]
