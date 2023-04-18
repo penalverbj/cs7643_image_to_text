16fc26edb06bb61a8bea85a3b003d5c5ddccfe19
Christian Smith <csmith657@gatech.edu>
2023-04-18 17:13:06 -0600

    Added validation set to download.sh COCO

--- data/download.sh
+++ data/download.sh
@@ -31,6 +31,9 @@ if [ ! -d "./coco" ]; then
     curl -O http://images.cocodataset.org/zips/train2017.zip
     unzip -q train2017.zip
     rm train2017.zip
+    curl -O http://images.cocodataset.org/annotations/val2017.zip
+    unzip -q val2017.zip
+    rm val2017.zip
     curl -O http://images.cocodataset.org/annotations/annotations_trainval2017.zip
     unzip -q annotations_trainval2017.zip
     rm annotations_trainval2017.zip
