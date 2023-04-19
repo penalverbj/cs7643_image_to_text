53d187f1063db5d10cb1947da8da65e10081c1e5
jpb6 <jpb6@gatech.edu>
2023-04-19 18:17:53 -0400

    Made basic tester for teacher

--- src/teacher.py
+++ src/teacher.py
@@ -1,11 +1,11 @@
 import requests
 from PIL import Image
-from datasets import load_dataset
+# from datasets import load_dataset
 
 from transformers import GPT2TokenizerFast, ViTImageProcessor, VisionEncoderDecoderModel
 
-class Teacher(data_set="", captioning_model="nlpconnect/vit-gpt2-image-captioning"):
-    def __init__(self):
+class Teacher():
+    def __init__(self, data_set="", captioning_model="nlpconnect/vit-gpt2-image-captioning"):
         # load a fine-tuned image captioning model and corresponding tokenizer and image processor
         self.model = VisionEncoderDecoderModel.from_pretrained(captioning_model)
         self.tokenizer = GPT2TokenizerFast.from_pretrained(captioning_model)
@@ -19,13 +19,29 @@ class Teacher(data_set="", captioning_model="nlpconnect/vit-gpt2-image-captionin
     def get_pixels_single_image(self):
         url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         image = Image.open(requests.get(url, stream=True).raw)
-        pixel_values = image_processor(image, return_tensors="pt").pixel_values
+        pixel_values = self.image_processor(image, return_tensors="pt").pixel_values
+        return pixel_values
 
     def caption_single_image(self):
-        generated_ids = model.generate(pixel_values)
-        generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        generated_ids = self.model.generate(self.get_pixels_single_image())
+        generated_text = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
         print(generated_text)
 
+
+def test():
+    teacher = Teacher()
+    encoder = teacher.model.encoder
+    decoder = teacher.model.decoder
+    pixel_values = teacher.get_pixels_single_image()
+    test_encoded = encoder(pixel_values)
+    print(test_encoded.last_hidden_state.shape)
+    print(test_encoded.pooler_output.shape)
+    test_decoded = decoder.generate(inputs_embeds=test_encoded.last_hidden_state)
+    decode = teacher.tokenizer.batch_decode(test_decoded, skip_special_tokens=True)[0]
+    print(decode)
+
+if __name__ == "__main__":
+    test()
 
 """
 This will implement the forward pass of our teacher model
